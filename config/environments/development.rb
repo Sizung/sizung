e4c7600b53959cea8d810794d344849def6d1b60
gugl <gugl@guenterglueck.com>
2015-10-08 17:24:05 +0200

    Using letter_opener to open emails in the browser in the dev environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,4 +42,5 @@ Rails.application.configure do
   config.react.variant = :development
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
 end
