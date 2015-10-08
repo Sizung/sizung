ccb6e53b16f6364d2e8ba6a57ab669c7398e1b54
gugl <gugl@guenterglueck.com>
2015-10-08 17:11:47 +0200

    Setup production mailing with mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ Rails.application.configure do
   config.react.variant = :development
 
   config.action_mailer.default_url_options = { host: ENV['SIZUNG_HOST'] }
+
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'staging-sizung.herokuapp.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
