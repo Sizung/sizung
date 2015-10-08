bff4bb8cac7e0f0115a200573a6bff417412fa2f
gugl <gugl@guenterglueck.com>
2015-10-08 17:30:45 +0200

    Edit profile link

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
+
+  def name
+    email
+  end
 end
