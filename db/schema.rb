f4085e11bbbbada1372460a706c5e65a4bf96130
gugl <gugl@guenterglueck.com>
2015-10-08 15:37:06 +0200

    Using UUIDs as the primary key for users

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,9 @@ ActiveRecord::Schema.define(version: 20151008123447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
