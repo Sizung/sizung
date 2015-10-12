e4c10bb3a56885981d3d2cbf6e5a051fe0bdd50e
gugl <gugl@guenterglueck.com>
2015-10-12 16:59:36 +0200

    A user has to sign up to see conversations

--- test/controllers/conversations_controller_test.rb
+++ test/controllers/conversations_controller_test.rb
@@ -1,56 +1,59 @@
+# require 'minitest/spec'
+# require 'minitest/autorun'
 require 'test_helper'
 
-class ConversationsControllerTest < ActionController::TestCase
+# class ConversationsControllerTest < ActionController::TestCase
+describe ConversationsController do
   include Devise::TestHelpers
 
-  def setup
-    # @request.env['devise.mapping'] = Devise.mappings[:user]
-    # sign_in @conversation.organization.owner
+  describe 'visitor' do
+    it 'should not see any conversations' do
+      get :index
+      assert_response :redirect
+      assert_redirected_to new_user_session_path
+    end
   end
 
-  def test_index
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:conversations)
-  end
-
-  def test_new
-    get :new
-    assert_response :success
-  end
-
-  def test_create
-    assert_difference('Conversation.count') do
-      post :create, conversation: { organization_id: FactoryGirl.create(:organization).id, title: 'Our general discussion' }
+  describe 'signed in' do
+    setup do
+      @conversation = FactoryGirl.create :conversation
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+      sign_in @conversation.organization.owner
     end
 
-    assert_redirected_to conversation_path(assigns(:conversation))
-  end
-
-  def test_show
-    conversation = FactoryGirl.create(:conversation)
-    get :show, id: conversation
-    assert_response :success
-  end
-
-  def test_edit
-    conversation = FactoryGirl.create(:conversation)
-    get :edit, id: conversation
-    assert_response :success
-  end
-
-  def test_update
-    conversation = FactoryGirl.create(:conversation)
-    put :update, id: conversation, conversation: { organization_id: conversation.organization_id, title: conversation.title }
-    assert_redirected_to conversation_path(assigns(:conversation))
-  end
-
-  def test_destroy
-    conversation = FactoryGirl.create(:conversation)
-    assert_difference('Conversation.count', -1) do
-      delete :destroy, id: conversation
+    it 'lists all conversations' do
+      get :index
+      assert_response :success
+      assert_not_nil assigns(:conversations)
     end
 
-    assert_redirected_to conversations_path
+    it 'creates a new conversation' do
+      assert_difference('Conversation.count') do
+        post :create, conversation: { organization_id: FactoryGirl.create(:organization).id, title: 'Our general discussion' }
+      end
+
+      assert_redirected_to conversation_path(assigns(:conversation))
+    end
+
+    it 'shows a single conversation' do
+      conversation = FactoryGirl.create(:conversation)
+      get :show, id: conversation
+      assert_response :success
+    end
+
+    it 'updates an existing conversation' do
+      conversation = FactoryGirl.create(:conversation)
+      put :update, id: conversation, conversation: { organization_id: conversation.organization_id, title: conversation.title }
+      assert_redirected_to conversation_path(assigns(:conversation))
+    end
+
+    it 'destroys a conversation' do
+      conversation = FactoryGirl.create(:conversation)
+      assert_difference('Conversation.count', -1) do
+        delete :destroy, id: conversation
+      end
+
+      assert_redirected_to conversations_path
+    end
   end
 end
