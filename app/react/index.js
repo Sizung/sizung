b1c6bcff7b521ecbff75a382318d31713938b161
gugl <gugl@guenterglueck.com>
2015-10-21 13:04:31 +0200

    Moved Sizung components to new webpack structure for hot reload

--- app/react/index.js
+++ app/react/index.js
@@ -4,3 +4,8 @@ import HelloWorld from './components/hello-world';
 
 registerComponent('hello-world', HelloWorld);
 
+import CommentForm from './components/sizung/components/CommentForm.js'
+registerComponent('CommentForm', CommentForm);
+
+import ConversationRoot from './components/sizung/containers/ConversationRoot.js'
+registerComponent('ConversationRoot', ConversationRoot);
