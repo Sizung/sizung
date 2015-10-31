0ddf9c8592e204e1d1faf12c293a8ff170d0000a
Aniruddha Ujlambkar <ani@sizung.com>
2015-11-01 00:41:22 +0530

    CSS styling updates for comment, agenda item and deliverable react components

--- app/react/components/sizung/components/AgendaItem.js
+++ app/react/components/sizung/components/AgendaItem.js
@@ -7,12 +7,12 @@ class AgendaItem extends React.Component {
   render() {
     return <div className="row white-bg padding-sm-vertical margin-xs-vertical box-shadow">
         <div className="col-xs-12">
-          <strong className="pull-left">{ this.props.title }</strong>
-          <i className="pull-right" glyph="tag" />
+          <span className="col-xs-11 zero-padding" style={{textAlign: 'left'}}>{ this.props.title }</span>
+          <i className="col-xs-1 fa fa-tag zero-padding" style={{textAlign: 'right'}}/>
         </div>
         <div className="col-xs-12">
-          <small className="pull-left">{this.props.commentsSize} comments</small>
-          <small className="pull-right">#{this.props.conversationTitle}</small>
+          <small className="pull-left text-muted">{this.props.commentsSize} comments</small>
+          <small className="pull-right text-muted">#{this.props.conversationTitle}</small>
         </div>
       </div>;
   }
--- app/react/components/sizung/components/Deliverable.js
+++ app/react/components/sizung/components/Deliverable.js
@@ -6,12 +6,16 @@ class Deliverable extends React.Component {
     render() {
         return <div className="row white-bg padding-sm-vertical margin-xs-vertical box-shadow">
           <div className="col-xs-12">
-            <strong className="col-xs-11 zero-padding">{ this.props.body }</strong>
-            <i className="col-xs-1 fa fa-tasks zero-padding"></i>
+            <span className="col-xs-11 zero-padding" style={{textAlign: 'left'}}>{ this.props.body }</span>
+            <i className="col-xs-1 fa fa-tasks zero-padding" style={{textAlign: 'right'}}></i>
           </div>
-          <div className="col-xs-12">
-            <small className="pull-left col-xs-6 zero-padding">{ this.props.assignee }</small>
-            <small className="pull-right col-xs-6 zero-padding"># { this.props.conversationTitle }</small>
+          <div className="col-xs-12" style={{marginTop: '5px'}}>
+            <div className="pull-left col-xs-6 zero-padding text-muted">
+              <div className="circle-sm">
+                <span className="circle-text-sm">{this.props.assignee.split(' ')[0].charAt(0)+this.props.assignee.split(' ')[1].charAt(0)}</span>
+              </div>
+            </div>
+            <small className="pull-right col-xs-6 zero-padding text-muted" style={{textAlign: 'right'}}># { this.props.conversationTitle }</small>
           </div>
         </div>;
 
