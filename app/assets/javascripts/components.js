c514bfff321db4642f450a8ada1c10965411c0a4
gugl <gugl@guenterglueck.com>
2015-11-20 10:23:38 +0100

    Removed some unused code.

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -1,8 +1,5 @@
 window.React = require('react');
 window.Root  = require('./components/counter_example/containers/Root.js');
-window.SizungRoot = require('./components/sizung/containers/SizungRoot.js');
-window.AgendaRoot = require('./components/sizung/containers/AgendaRoot.js');
-window.DeliverableRoot = require('./components/sizung/containers/DeliverableRoot.js');
 window.ConversationRoot = require('./components/sizung/containers/ConversationRoot.js');
 
 
