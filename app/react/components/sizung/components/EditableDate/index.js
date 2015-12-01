fad93c06de793f2bbcfaed5d5a231c60de6a2938
gugl <gugl@guenterglueck.com>
2015-12-01 14:58:15 +0100

    Using a better react-datepicker.

--- app/react/components/sizung/components/EditableDate/index.js
+++ app/react/components/sizung/components/EditableDate/index.js
@@ -3,50 +3,25 @@ import CSSModules from 'react-css-modules';
 import styles from "./index.css";
 
 //TODO: Find a way to include those css files here instead of in the asset pipeline
-//import datePickerStyles from '../../../../../../node_modules/react-date-picker/index.css';
-//require('react-date-picker/base.css');
-//require('react-date-picker/theme/hackerone.css');
-import DatePicker from 'react-date-picker';
+//require('react-datepicker/dist/react-datepicker.css');
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
 
 @CSSModules(styles)
 class EditableDate extends React.Component {
   constructor() {
     super();
-    this.state = {edit: false};
-
-    this.handleChange     = this.handleChange.bind(this);
-    this.handleEditClick  = this.handleEditClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(newValue) {
+    newValue = newValue ? newValue.format('YYYY-MM-DD').toString() : null;
     this.props.onUpdate(newValue);
-    this.setState({edit: false})
-  }
-
-  handleEditClick(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    this.setState({edit: true})
-  }
-
-  toDateString(value, nullString = null) {
-    if (value === null) {
-      return nullString;
-    }
-    else {
-      return value;
-    }
   }
 
   textElement(persistedText) {
-    if (this.state.edit) {
-      var value = this.toDateString(persistedText);
-      return <DatePicker date={value} onChange={this.handleChange} />;
-    }
-    else {
-      var value = this.toDateString(persistedText, 'no date');
-      return <div styleName='persisted-text-container'>{value}<a styleName="edit-link" href="#" onClick={this.handleEditClick}><i className="fa fa-pencil" style={{marginLeft: '1em'}} /></a></div>;
-    }
+    var value = persistedText ? moment(persistedText) : null;
+    return <DatePicker selected={value} placeholderText="When is it due?" onChange={this.handleChange} dateFormat="YYYY-MM-DD"/>;
   }
 
   render() {
