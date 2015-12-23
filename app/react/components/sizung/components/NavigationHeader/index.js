9b427254ad5c7f7fb597b188903ea14b8d9476c6
Aniruddha Ujlambkar <ani@sizung.com>
2015-12-23 17:37:26 +0530

    Putting home icon as a place holder currently until the image issue of sizung logo on staging server is resolved

--- app/react/components/sizung/components/NavigationHeader/index.js
+++ app/react/components/sizung/components/NavigationHeader/index.js
@@ -43,7 +43,7 @@ class NavigationHeader extends Component {
               <ul styleName='organisation-dropdown-nav'>
                 <li>
                   <a onClick={this.handleCurrentOrganizationClick} styleName='organisation-dropdown' href="#" data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>
-                    <img styleName='logo' src={'/assets/sizung_logo_white_on_black.gif'}/>
+                    <i className='fa fa-home fa-2x'></i>
                   </a>
                   <ul className="dropdown-menu">
                     {organizationElements}
