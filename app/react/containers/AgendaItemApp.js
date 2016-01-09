8c1ab22f444b0982c27ff3669240a8d5df14a9e7
gugl <gugl@guenterglueck.com>
2016-01-09 02:21:53 +0100

    Code cleanup for AgendaItemApp

--- app/react/containers/AgendaItemApp.js
+++ app/react/containers/AgendaItemApp.js
@@ -10,82 +10,67 @@ import * as ConversationObjectsActions from '../actions/conversationObjects';
 import * as selectors from '../utils/selectors';
 
 import ConversationObjectList from '../components/ConversationObjectList';
-import { fillConversationObject, fillAgendaItem } from '../utils/entityUtils';
+import { fillAgendaItem } from '../utils/entityUtils';
 
 class AgendaItemApp extends React.Component {
-  /*
-   * This is called when the component is first mounted to the DOM.
-   *
-   * Here we fetch the conversation Objects for the agendaItemId (from the route) and set the selectedAgendaItemId in
-   * the store using the selectAgendaItem ActionCreator. That is the same ActionCreator that is used when you click
-   * on an agenda item to select it.
-   */
   componentDidMount() {
-    const { conversationId, agendaItemId } = this.props.params;
-    this.props.selectAgendaItem(conversationId, agendaItemId);
+    this.fetchData();
   }
 
   componentDidUpdate(prevProps) {
-    const { conversationId, agendaItemId } = this.props.params;
-    const oldId = prevProps.params.agendaItemId;
-
-    if (agendaItemId !== oldId) {
-      this.props.selectAgendaItem(conversationId, agendaItemId);
+    if (this.props.params.agendaItemId !== prevProps.params.agendaItemId) {
+      this.fetchData();
     }
   }
 
+  fetchData = () => {
+    const { conversationId, agendaItemId } = this.props.params;
+    this.props.selectAgendaItem(conversationId, agendaItemId);
+  };
+
   render() {
-    const { conversationObjectsList } = this.props;
-    if (conversationObjectsList.conversationObjects) {
-      return (
-        <ConversationObjectList
-          {...this.props}
-          {...conversationObjectsList}
-        />
-      );
-    }
+    if (this.props.conversationObjects) { return <ConversationObjectList {...this.props} />; }
 
     return <div className="text-center"><h5>Loading Agenda Item...</h5></div>;
   }
 }
 
-function prepareConversationObjectList(state, objectsToShow, parentObject, canCreateAgendaItem, canCreateDeliverable) {
-  const currentUser = state.getIn(['entities', 'users', state.getIn(['currentUser', 'id'])]);
-  const conversationObjectsList = {commentForm: {}};
+function selectedAgendaItem(state) {
+  const selectedAgendaItemId = state.getIn(['selectedConversationObject', 'type']) === 'agendaItems' ? state.getIn(['selectedConversationObject', 'id']) : null;
+  return selectedAgendaItemId ? fillAgendaItem(state, selectedAgendaItemId) : null;
+}
 
-  if (objectsToShow) {
-    conversationObjectsList.conversationObjects = objectsToShow.get('references').map((objectReference) => {
-      return fillConversationObject(state, objectReference);
-    }).toList().sortBy((conversationObject) => {
-      return conversationObject.createdAt;
-    }).toJS();
+function objectsToShow(state) {
+  const selectedAgendaItemId = state.getIn(['selectedConversationObject', 'type']) === 'agendaItems' ? state.getIn(['selectedConversationObject', 'id']) : null;
+  return state.getIn(['conversationObjectsByAgendaItem', selectedAgendaItemId]);
+}
 
-    if (parseInt(decodeURIComponent(objectsToShow.get('nextPageUrl')).split("page[size]=")[1]) == 0) {
-      conversationObjectsList.nextPageUrl = null;
-    } else {
-      conversationObjectsList.nextPageUrl = objectsToShow.get('nextPageUrl');
-    }
-    conversationObjectsList.isFetching = objectsToShow.get('isFetching');
-    conversationObjectsList.commentForm.currentUser = currentUser;
-    conversationObjectsList.commentForm.parent = parentObject;
-    conversationObjectsList.commentForm.canCreateAgendaItem = canCreateAgendaItem;
-    conversationObjectsList.commentForm.canCreateDeliverable = canCreateDeliverable;
-    conversationObjectsList.canCreateAgendaItem = canCreateAgendaItem;
-    conversationObjectsList.canCreateDeliverable = canCreateDeliverable;
-    conversationObjectsList.users = selectors.conversationMembers(state);
+function isFetching(state) {
+  const list = objectsToShow(state);
+  return list ? list.get('isFetching') : false;
+}
+
+function nextPageUrl(state) {
+  const list = objectsToShow(state);
+  if (list && parseInt(decodeURIComponent(list.get('nextPageUrl')).split("page[size]=")[1]) !== 0) {
+    return list.get('nextPageUrl');
   }
-
-  return conversationObjectsList;
 }
 
 function mapStateToProps(state) {
-  const selectedAgendaItemId = state.getIn(['selectedConversationObject', 'type']) === 'agendaItems' ? state.getIn(['selectedConversationObject', 'id']) : null;
-  const objectsToShow = state.getIn(['conversationObjectsByAgendaItem', selectedAgendaItemId]);
-  const selectedAgendaItem = selectedAgendaItemId ? fillAgendaItem(state, selectedAgendaItemId) : null;
-  const conversationObjectsList = prepareConversationObjectList(state, objectsToShow, selectedAgendaItem, false, true);
-
   return {
-    conversationObjectsList,
+    conversationObjects: selectors.conversationObjects(state, objectsToShow(state)),
+    commentForm: {
+      currentUser: selectors.currentUser(state),
+      parent: selectedAgendaItem(state),
+      canCreateAgendaItem: false,
+      canCreateDeliverable: true,
+    },
+    users: selectors.conversationMembers(state),
+    canCreateAgendaItem: false,
+    canCreateDeliverable: true,
+    isFetching: isFetching(state),
+    nextPageUrl: nextPageUrl(state),
     currentConversation: selectors.currentConversation(state),
     conversationMembers: selectors.conversationMembers(state),
   };
--- app/react/utils/selectors.js
+++ app/react/utils/selectors.js
@@ -1,8 +1,22 @@
+import { fillConversationObject } from './entityUtils';
+
 const currentUser = (state) => state.getIn(['entities', 'users', state.getIn(['currentUser', 'id'])]);
 const currentConversation = (state) => state.getIn(['entities', 'conversations', state.getIn(['currentConversation', 'id'])]);
 const currentOrganization = (state) => state.getIn(['entities', 'organizations', state.getIn(['currentOrganization', 'id'])]);
 const organizations = (state) => state.getIn(['entities', 'organizations']).map((organization) => { return organization; }).toList();
 const conversationMembers = (state) => state.getIn(['entities','conversationMembers']);
+
+const conversationObjects = (state, objectsToShow) => {
+  if (!objectsToShow) {
+    return null;
+  }
+
+  return objectsToShow.get('references').map((objectReference) => {
+    return fillConversationObject(state, objectReference);
+  }).toList().sortBy((conversationObject) => {
+    return conversationObject.createdAt;
+  }).toJS();
+};
 
 export {
   currentUser,
@@ -10,4 +24,5 @@ export {
   currentOrganization,
   organizations,
   conversationMembers,
+  conversationObjects,
 };
