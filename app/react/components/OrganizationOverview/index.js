0eaae68393d10acb428b551b2a277ba25eae960e
gugl <gugl@guenterglueck.com>
2016-01-18 12:36:08 +0100

    Using ConversationLayout for swipe mechanics

--- app/react/components/OrganizationOverview/index.js
+++ app/react/components/OrganizationOverview/index.js
@@ -4,6 +4,7 @@ import Organization from '../Organization';
 import Conversation from '../Conversation';
 import AgendaItemList from '../AgendaItemList';
 import DeliverableList from '../DeliverableList';
+import ConversationLayout from '../ConversationLayout';
 import CSSModules from 'react-css-modules';
 import styles from './index.css';
 
@@ -22,34 +23,62 @@ class OrganizationOverview extends Component {
 
     return (
       <div>
-        <div>
-          <Organization organization={organization}/>
-        </div>
-        <div className="row" styleName="root">
-          <div className="col-sm-3" styleName="left-panel">
-            <AgendaItemList agendaItems={ agendaItems } visitAgendaItem={ visitAgendaItem } />
+        <Organization organization={organization}/>
+        <ConversationLayout
+          left={ <AgendaItemList agendaItems={ agendaItems } visitAgendaItem={ visitAgendaItem } /> }
+          right={ <DeliverableList deliverables={ deliverables } selectDeliverable={ selectDeliverable } /> }
+        >
+          <div styleName='header'>
+            <h5 style={{ margin: '5px', fontWeight: 'bold' }}>
+              <span className='pull-right'>
+                <a href={'/organizations/' + organization.id + '/conversations/new'}>
+                  <i className="fa fa-plus" />
+                  Add Conversation
+                </a>
+              </span>
+              CONVERSATIONS
+            </h5>
           </div>
-          <div className="col-xs-12 col-sm-6" styleName="center-panel">
-            <div styleName='header'>
-              <h5 style={{margin: '5px', fontWeight: 'bold'}}>
-                <span className='pull-right'>
-                  <a href={'/organizations/' + organization.id + '/conversations/new'}>
-                    <i className="fa fa-plus" />
-                    Add Conversation
-                  </a>
-                </span>
-                CONVERSATIONS
-              </h5>
-            </div>
-            { this.conversationElements(conversations) }
-          </div>
-          <div className="col-sm-3" styleName="right-panel">
-            <DeliverableList deliverables={ deliverables } selectDeliverable={ selectDeliverable } />
-          </div>
-        </div>
+          { this.conversationElements(conversations) }
+        </ConversationLayout>
       </div>
     );
   }
+
+
+  //render() {
+  //  const { organization, conversations, agendaItems, visitAgendaItem, deliverables, selectDeliverable } = this.props;
+  //
+  //  return (
+  //    <div>
+  //      <div>
+  //        <Organization organization={organization}/>
+  //      </div>
+  //      <div className="row" styleName="root">
+  //        <div className="col-sm-3" styleName="left-panel">
+  //          <AgendaItemList agendaItems={ agendaItems } visitAgendaItem={ visitAgendaItem } />
+  //        </div>
+  //        <div className="col-xs-12 col-sm-6" styleName="center-panel">
+  //          <div styleName='header'>
+  //            <h5 style={{margin: '5px', fontWeight: 'bold'}}>
+  //              <span className='pull-right'>
+  //                <a href={'/organizations/' + organization.id + '/conversations/new'}>
+  //                  <i className="fa fa-plus" />
+  //                  Add Conversation
+  //                </a>
+  //              </span>
+  //              CONVERSATIONS
+  //            </h5>
+  //          </div>
+  //          { this.conversationElements(conversations) }
+  //        </div>
+  //        <div className="col-sm-3" styleName="right-panel">
+  //          <DeliverableList deliverables={ deliverables } selectDeliverable={ selectDeliverable } />
+  //        </div>
+  //      </div>
+  //    </div>
+  //  );
+  //}
 }
 
 OrganizationOverview.propTypes = {
