70bb89bfead17db49810d77a976299955b277556
Aniruddha Ujlambkar <ani@sizung.com>
2016-01-18 23:57:31 +0530

    Organization view fixed for handling the variable height Agenda list, Deliverable list and conversation list Code written in jquery to do dynamic DOM manipulation ... need to discuss about that on next call to find the best way to handle such cases Current changes could be termed as workaround if we find any better way to handle it

--- app/react/components/OrganizationOverview/index.js
+++ app/react/components/OrganizationOverview/index.js
@@ -10,6 +10,28 @@ import styles from './index.css';
 
 @CSSModules(styles)
 class OrganizationOverview extends Component {
+  constructor() {
+    super();
+
+    this.handleResize = this.handleResize.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentDidUpdate() {
+    this.handleResize();
+  }
+
+  handleResize() {
+    console.log('Resizing!');
+    const organizationNode = React.findDOMNode(this.refs.organization);
+    const rootNode = React.findDOMNode(this.refs.root);
+    if (organizationNode !== null && rootNode !== null) {
+      $(rootNode).css('padding-bottom',$(organizationNode).outerHeight(true)+'px');
+    }
+  }
   conversationElements = (conversations) => {
     const elements = conversations.map((conversation) => {
       return <Conversation key={ conversation.id } conversation={ conversation }/>;
@@ -22,24 +44,28 @@ class OrganizationOverview extends Component {
     const { organization, conversations, agendaItems, visitAgendaItem, deliverables, selectDeliverable } = this.props;
 
     return (
-      <div styleName='root'>
-        <Organization organization={organization}/>
+      <div styleName='root' ref='root'>
+        <Organization organization={organization} ref='organization'/>
         <ConversationLayout
           left={ <AgendaItemList agendaItems={ agendaItems } visitAgendaItem={ visitAgendaItem } /> }
           right={ <DeliverableList deliverables={ deliverables } selectDeliverable={ selectDeliverable } /> }
         >
-          <div styleName='header'>
-            <h5 style={{ margin: '5px', fontWeight: 'bold' }}>
-              <span className='pull-right'>
-                <a href={'/organizations/' + organization.id + '/conversations/new'}>
-                  <i className="fa fa-plus" />
-                  Add Conversation
-                </a>
-              </span>
-              CONVERSATIONS
-            </h5>
+          <div styleName='center-panel'>
+            <div styleName='header'>
+              <h5 style={{ margin: '5px', fontWeight: 'bold' }}>
+                <span className='pull-right'>
+                  <a href={'/organizations/' + organization.id + '/conversations/new'}>
+                    <i className="fa fa-plus" />
+                    Add Conversation
+                  </a>
+                </span>
+                CONVERSATIONS
+              </h5>
+            </div>
+            <div styleName='center-panel-list'>
+              { this.conversationElements(conversations) }
+            </div>
           </div>
-          { this.conversationElements(conversations) }
         </ConversationLayout>
       </div>
     );
