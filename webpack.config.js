a96c298ea20fb9114163f7f2c6aba144a40796ff
gugl <gugl@guenterglueck.com>
2016-01-20 13:46:51 +0100

    Fixed the css problem on staging

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
       {
         test: /\.css$/,
         loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]')
-        //loader: 'style-loader!css-loader?modules'
+        // loader: 'style-loader!css-loader?modules'
       }
     ]
   },
