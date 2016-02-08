420c03448346754b96c6f0084e35967263abc895
Aniruddha Ujlambkar <ani@sizung.com>
2016-02-08 16:24:35 +0530

    Fixed: Issue related to conversation title appearing on top navigation in Organization view

--- app/react/containers/ApplicationLayoutApp.js
+++ app/react/containers/ApplicationLayoutApp.js
@@ -6,7 +6,8 @@ function mapStateToProps(state, props) {
   return {
     organizations: selectors.organizations(state),
     currentOrganization: selectors.currentOrganization(state),
-    currentConversation: selectors.currentConversation(state),
+    // TODO: This is a quick fix due to broken visual hierarchy. Do it the clean way when the visual hierarchy is corrected.
+    currentConversation: props.params.organizationId ? null : selectors.currentConversation(state),
     currentUser: selectors.currentUser(state),
     users: selectors.conversationMembers(state),
   };
