3f9d17f28421b14a52e3ab59f163fd8b3c2b2918
Aniruddha Ujlambkar <ani@sizung.com>
2016-02-10 13:10:03 +0530

    Meeting invitation mailer generated

--- app/mailers/meeting_mailer.rb
+++
@@ -1,7 +0,0 @@
-class MeetingMailer < ApplicationMailer
-  def user_called_for_meeting(user)
-    @user = user
-
-    mail to: user.email
-  end
-end
---
+++ test/mailers/previews/meeting_invitation_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/meeting_invitation_mailer
+class MeetingInvitationMailerPreview < ActionMailer::Preview
+
+end
