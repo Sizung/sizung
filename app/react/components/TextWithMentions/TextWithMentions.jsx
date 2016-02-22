f0e4e8ce5c15d739888ddfcc0c892d00c95d0503
gugl <gugl@guenterglueck.com>
2016-02-22 16:29:45 +0100

    Fixed mentions in AgendaItem and Deliverable titles. Also with a maxLength of 40 chars

--- app/react/components/TextWithMentions/TextWithMentions.jsx
+++ app/react/components/TextWithMentions/TextWithMentions.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import marked from 'marked';
+import styles from './TextWithMentions.css';
 
 class TextWithMentions extends React.Component {
+
+  static propTypes = {
+    maxLength: PropTypes.number,
+  };
 
   rawMarkup = () => {
     const pattern = new RegExp(/(.*)@\[(.*)\]\(.*\)(.*)/);
@@ -12,13 +17,17 @@ class TextWithMentions extends React.Component {
       text = text.replace(pattern, '$1__$2__$3');
     }
 
+    text = this.props.maxLength && text.length > this.props.maxLength ? text.substring(0, this.props.maxLength) + '...' : text;
+
     const rawMarkup = marked(text, { sanitize: true });
     return { __html: rawMarkup };
   };
 
   render() {
+    if (!this.props.children) { return <span></span>; }
+
     return (
-      <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      <span className={styles.root} dangerouslySetInnerHTML={this.rawMarkup()} />
     );
   }
 }
