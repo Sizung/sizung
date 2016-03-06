6ebe43eae6604b686af111fbcb33198d571061f8
Aniruddha Ujlambkar <ani@sizung.com>
2016-03-06 13:27:58 +0530

    Deliverable Icon component code cleanup

--- app/react/components/DeliverableIcon/index.js
+++ app/react/components/DeliverableIcon/index.js
@@ -3,29 +3,18 @@ import styles from './index.css';
 
 class DeliverableIcon extends React.Component {
 
-  constructor() {
-    super();
-    this.validSizes = ['normal', 'large'];
-  }
   render() {
     const iconStyle = [];
     const { inverted, size, status } = this.props;
-    const iconSize = (this.validSizes.indexOf(size) === -1) ? this.validSizes[0] : this.validSizes[this.validSizes.indexOf(size)];
+    const iconSize = (styles[size] ? size : 'normal');
+    const iconStatus = (styles[status] ? status : 'default');
     if (inverted) {
       iconStyle.push(styles.inverted);
     } else {
-      if (status === 'overdue') {
-        iconStyle.push(styles.overdue);
-      } else if (status === 'dueToday') {
-        iconStyle.push(styles.dueToday);
-      } else if (status === 'completed') {
-        iconStyle.push(styles.completed);
-      } else {
-        iconStyle.push(styles.default);
-      }
+      iconStyle.push(styles[iconStatus]);
     }
+    iconStyle.push(styles[iconSize]);
 
-    iconStyle.push(iconSize === 'normal' ? styles.normal : styles.large);
     return (
       <div className={iconStyle.join(' ')}></div>
     );
