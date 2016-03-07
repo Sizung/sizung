ec24ee243afef326f944014a102246b447a2a25a
Aniruddha Ujlambkar <ani@sizung.com>
2016-03-08 00:46:12 +0530

    Editable Date and date picker customisation as per designs (few scenarios yet need to be handled by design team and will be conveyed soon)

--- app/react/components/EditableDate/index.js
+++ app/react/components/EditableDate/index.js
@@ -25,8 +25,11 @@ class EditableDate extends React.Component {
                        placeholderText="Due On?"
                        onChange={this.handleChange}
                        dateFormat="DD MMM - YYYY"
-                       weekStart='0'
-                       disabled={!editable} />;
+                       weekStart='1'
+                       disabled={!editable}
+                       locale={'en'}
+                       weekdays={"S_M_T_W_T_F_S".split("_")}
+                       />;
   }
 
   render() {
