e93d102c1dd6ba5b9af362694bafcd139abbaffc
Aniruddha Ujlambkar <ani@sizung.com>
2016-03-08 19:56:49 +0530

    Mention Style added inside SizungInputApp ... however its usage is commented out for now

---
+++ app/react/components/SizungInput/mentionStyle.js
@@ -0,0 +1,38 @@
+export default (props={}) => ({
+  control: {
+    display: 'inline-block',
+    width: '100%',
+    minHeight: 40,
+    backgroundColor: '#fff',
+  },
+
+  highlighter: {
+    padding: 9,
+    border: '1px solid silver',
+  },
+
+  textarea: {
+    margin: 0,
+    padding: 9,
+    minHeight: 40,
+    border: '1px solid silver',
+    outline: 0,
+  },
+
+  input: {
+    padding: 1,
+    margin: 0,
+    border: '2px inset',
+  },
+
+  suggestions: {
+    border: '1px solid rgba(0,0,0,0.15)',
+    item: {
+      padding: '5px 15px',
+      borderBottom: '1px solid rgba(0,0,0,0.15)',
+      '&focused': {
+        backgroundColor: '#cee4e5',
+      },
+    },
+  },
+});
