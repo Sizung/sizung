ceac0b6afab1c71443b7ceb45b8f662cc92e2e05
gugl <gugl@guenterglueck.com>
2016-03-11 12:42:33 +0100

    Due on property for the deliverable composer

--- app/react/components/DeliverableComposer/DeliverableComposer.jsx
+++ app/react/components/DeliverableComposer/DeliverableComposer.jsx
@@ -4,6 +4,7 @@ import SizungInputApp from '../../containers/SizungInputApp';
 import CloseIcon from '../CloseIcon';
 import DeliverableIcon from '../DeliverableIcon';
 import EditableUserApp from '../../containers/EditableUserApp';
+import EditableDate from '../EditableDate';
 
 class DeliverableComposer extends React.Component {
   static propTypes = {
@@ -17,14 +18,14 @@ class DeliverableComposer extends React.Component {
 
   constructor() {
     super();
-    this.state = { value: '' };
+    this.state = { value: '', assigneeId: null, dueOn: null };
   }
 
   handleSubmit = (e) => {
     const title = this.state.value.trim();
-    const { assigneeId } = this.state;
+    const { assigneeId, dueOn } = this.state;
     if (title === '') { return; } // TODO: Improve that quickfix when the whole new ui behavior gets implemented
-    this.props.createDeliverable({ agenda_item_id: this.props.parent.id, title, assigneeId });
+    this.props.createDeliverable({ agenda_item_id: this.props.parent.id, title, assignee_id: assigneeId, due_on: dueOn });
     this.setState({ value: '' });
   };
 
@@ -36,7 +37,13 @@ class DeliverableComposer extends React.Component {
     this.setState({ assigneeId });
   }
 
+  handleDueOnUpdate = (dueOn) => {
+    this.setState({ dueOn });
+  }
+
   render() {
+    const { dueOn } = this.state;
+
     return (
       <div className={styles.root}>
         <div className={styles.row}>
@@ -47,14 +54,20 @@ class DeliverableComposer extends React.Component {
           <CloseIcon onClick={this.props.onClose} />
         </div>
         <div className={styles.properties}>
-          <div>ASSIGN TO</div>
-          <div>
+          <div className={styles.assigneeContainer}>
+            <div className={styles.assignLabel}>ASSIGN TO</div>
             <EditableUserApp userId={this.state.assigneeId} conversationId={this.props.parent.conversationId} editable direction="north" onUpdate={this.handleAssigneeUpdate} />
+          </div>
+          <div className={styles.dueOnContainer}>
+            <div className={styles.dueOnLabel}>DUE ON</div>
+            <div className={styles.dueOnWrapper}>
+              <EditableDate value={dueOn} onUpdate={this.handleDueOnUpdate} editable />
+            </div>
           </div>
         </div>
         <div className={styles.row}>
-          <DeliverableIcon />
           <form className={styles.form} onSubmit={this.handleSubmit}>
+            <DeliverableIcon inverted size="xLarge" />
             <SizungInputApp ref="name" onChange={this.handleChangeInMentionBox} onSubmit={this.handleSubmit} value={this.state.value} rows="1" placeholder="Type your deliverable here" />
           </form>
         </div>
