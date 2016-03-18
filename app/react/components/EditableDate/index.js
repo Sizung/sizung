e399e3c26ecc5029619063f5af4ed57b43bec7cf
gugl <gugl@guenterglueck.com>
2016-03-18 15:57:00 +0100

    Fixed styling for due on datepicker

--- app/react/components/EditableDate/index.js
+++ app/react/components/EditableDate/index.js
@@ -1,31 +1,35 @@
-import React, { Component, PropTypes } from 'react';
-import CSSModules from 'react-css-modules';
-import styles from "./index.css";
+import React, { PropTypes } from 'react';
+import styles from './index.css';
 
-//TODO: Find a way to include those css files here instead of in the asset pipeline
-//require('react-datepicker/dist/react-datepicker.css');
+// TODO: Find a way to include those css files here instead of in the asset pipeline
+// require('react-datepicker/dist/react-datepicker.css');
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 
-@CSSModules(styles)
 class EditableDate extends React.Component {
-  constructor() {
-    super();
-    this.handleChange = this.handleChange.bind(this);
-  }
 
-  handleChange(newValue) {
-    newValue = newValue ? newValue.format('YYYY-MM-DD').toString() : null;
-    this.props.onUpdate(newValue);
+  static propTypes = {
+    editable: PropTypes.bool,
+    value: PropTypes.string,
+    onUpdate: PropTypes.func.isRequired,
+  };
+
+  static defaultProps = {
+    editable: true,
+  };
+
+  handleChange = (newValue) => {
+    const value = newValue ? newValue.format('YYYY-MM-DD').toString() : null;
+    this.props.onUpdate(value);
   }
 
   textElement(persistedText, editable) {
-    var value = persistedText ? moment(persistedText) : null;
+    const value = persistedText ? moment(persistedText) : null;
     return <DatePicker selected={value}
                        placeholderText="Due On?"
                        onChange={this.handleChange}
                        dateFormat="DD MMM - YYYY"
-                       weekStart='1'
+                       weekStart="1"
                        disabled={!editable}
                        locale={'en'}
                        weekdays={"S_M_T_W_T_F_S".split("_")}
@@ -33,19 +37,9 @@ class EditableDate extends React.Component {
   }
 
   render() {
-    const {value, editable} = this.props;
+    const { value, editable } = this.props;
     return this.textElement(value, editable);
   }
 }
-
-EditableDate.propTypes = {
-  editable: PropTypes.bool,
-  value: PropTypes.string,
-  onUpdate: PropTypes.func.isRequired
-};
-
-EditableDate.defaultProps = {
-  editable: true
-};
 
 export default EditableDate;
