6faf84c423122f27f8b41c4993a43fd4fa6c9439
Aniruddha Ujlambkar <ani@sizung.com>
2016-03-18 15:15:21 +0530

    Conversation header component updated for Responsive CSS along with related child components wherever necessary

--- app/react/components/ConversationMembersCounter/ConversationMembersCounter.jsx
+++ app/react/components/ConversationMembersCounter/ConversationMembersCounter.jsx
@@ -12,10 +12,8 @@ class ConversationMembersCounter extends React.Component {
     const usersCount = this.props.conversationMembers ? this.props.conversationMembers.toList().size : 0;
     return (
       <span className={styles.memberDropdownContainer} onClick={this.handleToggleView}>
-          <div aria-haspopup="true" aria-expanded="false">
-            <UserIcon inverted size={'x-large'} style={{ paddingTop: '15px' }} />
-            <div className={styles.memberBadge}>{usersCount}</div>
-          </div>
+        <UserIcon/>
+        <div className={styles.memberBadge}>{usersCount}</div>
       </span>
     );
   };
