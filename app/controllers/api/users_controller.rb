aad55cb37d15ff32c174fa3baf156996a60dda0d
Aniruddha Ujlambkar <ani@sizung.com>
2016-04-10 16:56:57 +0530

    Basic first implementation of Settings Component added with "Profile" settings option operational Update action of user controller needs to add authorization checks Settings Component Route needs to be cross verified so that it is in line with existing route convention Updates through Settings component need to be updated to have them go through redux store ( In progress )

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,7 @@
 module Api
   class UsersController < ApplicationController
     skip_before_action :verify_authenticity_token, only: [:create]
+    before_action :set_user, only: [:update]
     respond_to :json
 
     def index
@@ -14,8 +15,6 @@ module Api
 
     def create
       @user = User.create!(user_params)
-      # puts 'Parameters Passed'
-      # puts params.inspect
       organization_name = Organization::DEFAULT_NAME
       if params[:user][:organization][:name]
         organization_name = params[:user][:organization][:name]
@@ -26,9 +25,28 @@ module Api
       render json: @user, serializer: UserSerializer
     end
 
+    def update
+      # authorize @user
+      @user.update!(update_user_params)
+      render json: @user, serializer: UserSerializer
+    end
+
     private
       def user_params
         params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
+      end
+
+      def update_user_params
+        if !params[:user][:password].blank? && !params[:user][:password_confirmation].blank?
+          params.require(:user).permit(:first_name, :last_name, :password, :password_confirmation)
+        else
+          params.require(:user).permit(:first_name, :last_name)
+        end
+      end
+
+      def set_user
+        @user = User.find(params[:id])
+        # authorize @user
       end
 
   end
---
+++ app/react/components/Settings/index.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import styles from './index.css';
+import ProfileSettings from '../ProfileSettings';
+import OrganizationSettings from '../OrganizationSettings';
+import * as api from '../../utils/api';
+
+class Settings extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      currentOption: 0,
+      user: {
+        email: '',
+        firstName: '',
+        lastName: '',
+        password: '',
+        passwordConfirmation: '',
+      },
+    };
+  }
+
+  setCurrentSettingOption = (option) => {
+    this.setState({
+      currentOption: option,
+    });
+  };
+
+  setUser = (user) => {
+    this.setState({
+      user: {
+        email: ((user.email !== undefined) ? user.email : this.state.user.email),
+        firstName: ((user.firstName !== undefined) ? user.firstName : this.state.user.firstName),
+        lastName: ((user.lastName !== undefined) ? user.lastName : this.state.user.lastName),
+        password: ((user.password !== undefined) ? user.password : this.state.user.password),
+        passwordConfirmation: ((user.passwordConfirmation !== undefined) ? user.passwordConfirmation : this.state.user.passwordConfirmation),
+      },
+    });
+  };
+
+  saveUser = (user) => {
+    api.putJson('/api/users/'+this.props.currentUser.id, user, (json) => {
+      alert('user updated successfully');
+    });
+  };
+
+  renderOptions = () => {
+    if (this.state.currentOption === 0) {
+      return (<ProfileSettings user={this.state.user} setUser={this.setUser} onSave={this.saveUser}/>);
+    } else if (this.state.currentOption === 1) {
+      return (<OrganizationSettings/>);
+    }
+  };
+
+  getOptionLabelStyle = (option) => {
+    if (option === this.state.currentOption) {
+      return styles.currentOption;
+    }
+    return styles.option;
+  };
+
+  renderOptionLabel = (option) => {
+    let optionLabel = '';
+    const optionLabelStyle = this.getOptionLabelStyle(option);
+    if (option === 0) {
+      optionLabel = 'Profile';
+    } else if (option === 1) {
+      optionLabel = 'Teams';
+    }
+    return (
+      <div className={optionLabelStyle} onClick={this.setCurrentSettingOption.bind(this, option)}>
+          <span className={styles.optionLabel}>
+            {optionLabel}
+          </span>
+          <span className={styles.optionIndicator}>
+          </span>
+      </div>
+    );
+  };
+
+  componentWillMount() {
+    this.setUser(this.props.currentUser);
+  }
+
+  render() {
+    return (
+      <div className={styles.root}>
+        <div className={styles.leftColumn}>
+          <div className={styles.header}>
+            <div className={styles.settingsLogo}>
+            </div>
+            <div className={styles.settingsLabel}>
+              SETTINGS
+            </div>
+          </div>
+          <div className={styles.optionsContainer}>
+            {this.renderOptionLabel(0)}
+            {this.renderOptionLabel(1)}
+          </div>
+        </div>
+        <div className={styles.rightColumn} >
+          {this.renderOptions()}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Settings;
