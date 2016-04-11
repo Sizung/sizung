6859c7af8da3178718d275f10d374d95b3c20bf8
Aniruddha Ujlambkar <ani@sizung.com>
2016-04-11 21:08:30 +0530

    Email taken validation was commented earlier for trouble shooting purposes ... no re-enabled "Create new team" link on Sign In page now linked to "signup component"

--- app/react/components/SignUpCredentials/index.js
+++ app/react/components/SignUpCredentials/index.js
@@ -42,15 +42,14 @@ class SignUpCredentials extends React.Component {
       const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       if (!emailRegex.test(email)) {
         errorMessage = 'Email Address is not valid';
+      } else {
+        api.fetchJson('/api/users?email=' + email, (json) => {
+          if (json.emailExists) {
+            errorMessage = 'This email address is already taken';
+          }
+          this.setState({ emailErrorMessage: errorMessage });
+        });
       }
-      //else {
-      //  api.fetchJson('/api/users?email=' + email, (json) => {
-      //    if (json.emailExists) {
-      //      errorMessage = 'This email address is already taken';
-      //    }
-      //    this.setState({ emailErrorMessage: errorMessage });
-      //  });
-      //}
     }
     this.setState({ emailErrorMessage: errorMessage });
 
