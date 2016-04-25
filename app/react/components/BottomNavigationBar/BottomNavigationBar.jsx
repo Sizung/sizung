865a0a0751f165aeae6fe7a53fe41b27a8be91aa
Aniruddha Ujlambkar <ani@sizung.com>
2016-04-25 22:35:49 +0530

    Basic CSS layout changes done and Bottom navigation bar added with actions to switch between the 3 column layout of the application from mobile screen perspective

---
+++ app/react/components/BottomNavigationBar/BottomNavigationBar.jsx
@@ -0,0 +1,43 @@
+import React, { PropTypes } from 'react';
+import styles from './BottomNavigationBar.css';
+import Icon from '../Icon';
+
+class BottomNavigationBar extends React.Component {
+
+  static propTypes = {
+    selectedOption: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+  };
+
+  handleClick = (option) => {
+    this.props.onChange(option);
+  };
+
+  render() {
+    const { selectedOption } = this.props;
+    return (
+      <div className={styles.root}>
+        <div className={ selectedOption === 'agendaItems' ? styles.optionSelected : styles.option } onClick={this.handleClick.bind(this, 'agendaItems')}>
+          <Icon type="agendaItem" className={styles.optionIcon}/>
+          <div className={styles.optionLabel}>
+            Agendas
+          </div>
+        </div>
+        <div className={ selectedOption === 'conversations' ? styles.optionSelected : styles.option } onClick={this.handleClick.bind(this, 'conversations')}>
+          <Icon type="chat" className={styles.optionIcon}/>
+          <div className={styles.optionLabel}>
+            Conversations
+          </div>
+        </div>
+        <div className={ selectedOption === 'deliverables' ? styles.optionSelected : styles.option } onClick={this.handleClick.bind(this, 'deliverables')}>
+          <Icon type="deliverable" className={styles.optionIcon}/>
+          <div className={styles.optionLabel}>
+            Deliverables
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BottomNavigationBar;
