a17d422fe004c3db6001e61c9db06cb48ac447cd
gugl <gugl@guenterglueck.com>
2016-05-05 18:56:56 +0200

    Notify users when they get deliverables assigned

--- test/mailers/notifications_test.rb
+++ test/mailers/notifications_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 describe Notifications do
+  
   it 'mentioned' do
     user = FactoryGirl.create :user
     actor = FactoryGirl.create :user
@@ -12,5 +13,20 @@ describe Notifications do
     value(mail.body.encoded).must_match "#{actor.name} mentioned you in"
     value(mail.body.encoded).must_match 'This is a mention test for Sam Sample'
     value(mail.body.encoded).must_match 'sampleurl.example.com'
+  end
+
+  it 'deliverable_assigned' do
+    actor       = FactoryGirl.create :user
+    assignee    = FactoryGirl.create :user
+    deliverable = FactoryGirl.create :deliverable, assignee: assignee
+    mail        = Notifications.deliverable_assigned(deliverable, actor)
+    body        = mail.body.encoded
+
+    value(mail.to).must_equal [assignee.email]
+    value(mail.from).must_equal ['no-reply@sizung.com']
+    value(mail.subject).must_equal "#{actor.first_name} assigned a deliverable to you"
+    value(body).must_match "Hi #{assignee.first_name},"
+    value(body).must_match "#{actor.name} assigned you a deliverable: #{deliverable.title}"
+    value(body).must_match "http://localhost:3000/deliverables/#{deliverable.id}"
   end
 end
