e53fd4dc83db61df443915f555c60afc53d6f6c2
jpuri <jyotipuri@gmail.com>
2016-07-13 19:32:08 +0530

    Revert "removig commented code"
    This reverts commit 00a8a8a6f2b136950fd8affdba0453f4de134258.

--- app/react/components/ConversationObjectList/ConversationObjectList.jsx
+++ app/react/components/ConversationObjectList/ConversationObjectList.jsx
@@ -274,6 +274,13 @@ class ConversationObjectList extends Component {
     return undefined;
   };
 
+  // createNewComment = (obj) => {
+  //   this.setState({
+  //     newObjects: 0,
+  //   });
+  //   this.props.createComment(obj);
+  // };
+
   render() {
     const { createComment, createAgendaItem, createDeliverable, commentForm, createAttachment, archiveAttachment, params, labels } = this.props;
     const root = this.refs.root;
