5fbd44ec34185cc7b41ebd5d522d81f5ae404a14
gugl <gugl@guenterglueck.com>
2016-07-13 13:10:30 +0200

    Added api doc for unseen objects

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :name, :email, :presence_status, :created_at, :updated_at
+
+  include Swagger::Blocks
+
+  swagger_schema :reference_User do
+    property :data, required: [:id, :type] do
+      property :id, type: :string
+      property :type, type: :string, enum: ['users']
+    end
+  end
 end
