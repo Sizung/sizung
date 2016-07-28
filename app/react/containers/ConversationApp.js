cd37836f2144a034eb8632aec1459967105c35dd
jpuri <jyotipuri@gmail.com>
2016-07-28 14:44:16 +0530

    removing the extra api call to fetch conversation

--- app/react/containers/ConversationApp.js
+++ app/react/containers/ConversationApp.js
@@ -20,21 +20,6 @@ import * as AttachmentActions from '../actions/attachments';
 import * as labels from '../utils/entityLabels';
 
 class ConversationApp extends React.Component {
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.params.conversationId !== prevProps.params.conversationId) {
-      this.fetchData();
-    }
-  }
-
-  fetchData = () => {
-    const { conversationId } = this.props.params;
-    this.props.selectConversation(conversationId);
-  };
-
   render() {
     const { conversationObjects, commentForm, labels } = this.props;
     const { parent } = commentForm;
