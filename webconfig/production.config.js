10e2d0b14158d1bd8fffab08948b6cddbf4e7379
jpuri <jyotipuri@gmail.com>
2016-08-01 18:39:13 +0530

    moving jquery dependency to package.json

--- webconfig/production.config.js
+++ webconfig/production.config.js
@@ -34,11 +34,6 @@ module.exports = {
         __DEVTOOLS__: false
     }),
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
         warnings: false
