519f298b37cd6deb474bc05cbe4e5b811db86202
Aniruddha Ujlambkar <ani@sizung.com>
2016-10-12 19:09:46 +0530

    OrganizationDrowdown React component separated from TopBar Component

--- app/react/components/OrganizationDropdown/OrganizationDropdown.jsx
+++ app/react/components/OrganizationDropdown/OrganizationDropdown.jsx
@@ -1,54 +1,74 @@
 import React, { PropTypes } from 'react';
-import styles from './ProfileDropdown.css';
+import OrganizationIcon from '../OrganizationIcon/index';
+import ProfileDropdown from '../ProfileDropdown/index';
+import PlusIcon from '../PlusIcon';
+import styles from './OrganizationDropdown.css';
 
-class ProfileDropdown extends React.Component {
+class OrganizationDropdown extends React.Component {
   static propTypes = {
+    currentOrganization: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    }),
+    organizations: PropTypes.object.isRequired,
     currentUser: PropTypes.shape({
       name: PropTypes.string.isRequired,
     }).isRequired,
-    currentOrganization: PropTypes.object
+    reactLinks: PropTypes.bool,
+    currentConversation: PropTypes.object,
   };
 
-  static defaultProps = {};
+  static defaultProps = {
+    reactLinks: true,
+  };
 
-  renderOpen = () => {
-    const { name } = this.props.currentUser;
-    if (this.props.currentOrganization) {
-      return (
-          <div className={styles.rootOpen}>
-            <div className={styles.header}>
-              <div className={styles.round} title={name}>
-                {name.charAt(0)}
-              </div>
-              <div className={styles.name}>
-                {name}
-              </div>
-              <div className={styles.caretOpen}></div>
-            </div>
-            <div className={styles.menu}>
-              <a className={styles.settings} href={"/organizations/" + this.props.currentOrganization.id + "/settings"}>Settings</a>
-              <a href="/users/sign_out" data-method="delete">Sign Out</a>
-            </div>
-          </div>
-      );
+  otherOrganizationsUnseenCount = () => {
+    const { organizations, currentOrganization } = this.props;
+    let count = 0;
+    organizations.filter((org) => {
+      return org.id !== currentOrganization.id;
+    }).map((otherOrganization) => {
+      count += otherOrganization.unseenCount;
+    });
+    return count;
+  };
+
+  renderOrganizationList = () => {
+    const { organizations, currentOrganization, reactLinks } = this.props;
+
+    const otherOrganization = organizations.filter(org => org.id !== currentOrganization.id);
+    const unseenOrganization = otherOrganization
+        .filter(org => org.unseenCount > 0)
+        .sort(org => org.unseenCount);
+    const seenOrganization = otherOrganization.filter(org => org.unseenCount <= 0);
+
+    return unseenOrganization.concat(seenOrganization).map((org) => {
+      return <OrganizationIcon key={org.id} reactLink={reactLinks} organization={org} url={'/organizations/' + org.id} showUnseenNotification style={{ marginLeft: '2rem' }}/>;
+    });
+  };
+
+  renderUnseenNotification = () => {
+    if (this.otherOrganizationsUnseenCount() > 0) {
+      return <div className={styles.notification}/>;
     }
+    return null;
   };
 
   render() {
-    const { name } = this.props.currentUser;
-
+    const { currentOrganization, reactLinks } = this.props;
     return (
-      <div className={styles.hoverContainer}>
-        <div className={styles.rootShow}>
-          <div className={styles.round} title={name} >
-            {name.charAt(0)}
+        <div className={styles.organizationWrapper}>
+          {this.renderUnseenNotification()}
+          <OrganizationIcon organization={currentOrganization} reactLink={reactLinks} url={'/organizations/' + currentOrganization.id}/>
+          <div className={styles.otherOrganizations}>
+            {this.renderOrganizationList()}
+            <a href={'/organizations/new'} title={'+ New Organization'} style={{ marginLeft: '3rem' }}>
+              <PlusIcon/>
+            </a>
           </div>
-          <div className={styles.caretClosed}></div>
         </div>
-        {this.renderOpen()}
-      </div>
     );
-  }
+   }
 }
 
-export default ProfileDropdown;
+export default OrganizationDropdown;
--- app/react/components/OrganizationDropdown/index.js
+++ app/react/components/OrganizationDropdown/index.js
@@ -1,2 +1,2 @@
-import ProfileDropdown from './ProfileDropdown';
-export default ProfileDropdown;
+import OrganizationDropdown from './OrganizationDropdown';
+export default OrganizationDropdown;
